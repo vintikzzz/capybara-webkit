5d91d48e26f1460e63a6085c25f16b1b2f36dcc0
Pavel Tatarsky <fazzzenda@mail.ru>
2013-04-30 21:42:13 +0400

    added connection pool

--- lib/capybara/webkit.rb
+++ lib/capybara/webkit.rb
@@ -10,6 +10,13 @@ require "capybara/webkit/driver"
 Capybara.register_driver :webkit do |app|
   Capybara::Webkit::Driver.new(app)
 end
+Capybara.register_driver :permanent_webkit do |app|
+  ENV['WEBKIT_PORT'] ||= '40000'
+  ENV['WEBKIT_HOST'] ||= 'localhost'
+  connection = Capybara::Webkit::PermanentConnection.new(host: ENV['WEBKIT_HOST'], port: ENV['WEBKIT_PORT'].to_i)
+  browser = Capybara::Webkit::Browser.new(connection)
+  Capybara::Webkit::Driver.new(app, :browser => browser)
+end
 
 Capybara.register_driver :webkit_debug do |app|
   driver = Capybara::Webkit::Driver.new(app)
--- lib/capybara/webkit/permanent_connection.rb
+++ lib/capybara/webkit/permanent_connection.rb
@@ -4,62 +4,56 @@ require 'timeout'
 module Capybara::Webkit
   class PermanentConnection
 
+    def self.sockets
+      @sockets ||= {}
+    end
     attr_reader :port
 
     def initialize(options = {})
       @socket_class = options[:socket_class] || TCPSocket
       @port = options[:port] || 40000
+      @host = options[:host] || '127.0.0.1'
+    end
+
+    def socket
+      sockets = self.class.sockets
+      s = nil
+      if(!sockets.key?(@port))
+        Timeout.timeout(5) do
+          while s.nil?
+            begin
+              s = @socket_class.open(@host, @port)
+              if defined?(Socket::TCP_NODELAY)
+                s.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, true)
+              end
+              at_exit do
+                s.close
+              end
+            rescue Errno::ECONNREFUSED
+            end
+          end
+          sockets[@port] = s
+        end
+      else
+        s = sockets[@port]
+      end
+      s
     end
 
     def puts(string)
-      @socket.puts string
+      socket.puts string
     end
 
     def print(string)
-      @socket.print string
+      socket.print string
     end
 
     def gets
-      @socket.gets
+      socket.gets
     end
 
     def read(length)
-      @socket.read(length)
-    end
-
-    def connect
-      Timeout.timeout(5) do
-        while @socket.nil?
-          attempt_connect
-        end
-      end
-      if block_given?
-        begin
-          yield
-        rescue StandardError => e
-          begin
-            close
-          rescue StandardError => e2
-            raise e
-          end
-        end
-        close
-      end
-    end
-    private
-
-    def close
-      @socket.close
-    end
-    def attempt_connect
-      @socket = @socket_class.open("127.0.0.1", @port)
-      if defined?(Socket::TCP_NODELAY)
-        @socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, true)
-      end
-      at_exit do
-        close
-      end
-    rescue Errno::ECONNREFUSED
+      socket.read(length)
     end
   end
 end
